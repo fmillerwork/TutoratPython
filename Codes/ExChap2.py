431c423e47a670ef99f54729c21cfcdce6dfe002
fmillerwork <florian.miller.work@gmail.com>
2020-10-21 22:30:52 +0200

    ajout exos For

---
+++ Codes/ExChap2.py
@@ -0,0 +1,33 @@
+#Exo 12
+'''
+for i in range(1, 5):
+    print("*"*i)
+  '''  
+'''
+for i in range(4):
+    print("*"*(i+1))
+
+for i in range(5):
+    print("*"*i)
+'''
+print("---------------------")
+#Exo 13
+'''
+for i in range(4, 0, -1):
+    print("*"*i)
+'''
+'''
+for i in range(4, 0, -1):
+    print("*"*i)
+'''
+print("---------------------")
+#Exo 14
+
+for i in range(1, 5):
+    print(" " * (4-i), "*" * (2*i -1)," " * (4-i))
+for i in range(3,0,-1):
+    print(" " * (4-i), "*"* (2*i -1), " " * (4-i))
+
+for i in range(7):
+    print("*" * 1)
+#Exo 15
