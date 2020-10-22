e305758171167525a02e79ec49d2af76951af680
fmillerwork <florian.miller.work@gmail.com>
2020-10-22 09:43:01 +0200

    ajouts exos chap 3

--- Codes/ExChap2.py
+++ Codes/ExChap2.py
@@ -6,28 +6,32 @@ for i in range(1, 5):
 '''
 for i in range(4):
     print("*"*(i+1))
-
+'''
+'''
 for i in range(5):
     print("*"*i)
 '''
-print("---------------------")
+#print("---------------------")
+
 #Exo 13
 '''
 for i in range(4, 0, -1):
     print("*"*i)
 '''
-'''
-for i in range(4, 0, -1):
-    print("*"*i)
-'''
-print("---------------------")
-#Exo 14
+#print("---------------------")
 
+#Exo 14
+'''hauteur = 4 comme exemple
 for i in range(1, 5):
     print(" " * (4-i), "*" * (2*i -1)," " * (4-i))
 for i in range(3,0,-1):
     print(" " * (4-i), "*"* (2*i -1), " " * (4-i))
+'''
+'''
+hauteur=int(input("hauteur = "))
+for i in range(1, hauteur + 1):
+    print(" " * (hauteur-i), "*" * (2*i-1)," " * (hauteur-i))
+for i in range(hauteur-1,0,-1):
+    print(" " * (hauteur-i), "*"* (2*i -1)," " * (hauteur-i))
 
-for i in range(7):
-    print("*" * 1)
-#Exo 15
+'''
---
+++ Codes/ExChap3.py
@@ -0,0 +1,72 @@
+from random import random
+from random import randint
+
+#Ex2
+'''
+x = randint(1,50)
+y = randint(2,5)
+print("x =" ,x ,"y =" ,y , "x**y =",x**y)
+'''
+
+#Ex4
+#print(round(random()*10, 2))
+
+#Ex5
+'''
+for i in range(1,50+1):
+    print(randint(1,i))
+#erreur range(50) car 0
+'''
+#Ex9
+'''
+hour = int(input("Enter hour (1 to 12): "))
+duration = int(input("How many hours ahead ? "))
+
+print("New hour :", (hour + duration) % 12, "o'clock")
+'''
+#Ex11
+'''
+kilogrammes = eval(int("kilogrammes : "))
+print("pounds =", kilogrammes * 2.2046)
+'''
+#Ex12
+'''
+nb = int(input("nombre = "))
+factoriel = 1
+for i in range(1, nb + 1):
+    factoriel *= i
+print(factoriel)
+'''
+#Ex17
+#sans IF, compliqué
+
+
+#Ex18
+'''
+prix = eval(input("Prix : "))
+montantPaye = eval(input("Montant payé : "))
+montantRendu = round(montantPaye - prix, 2)
+print("Montant à rendre =", montantRendu)
+
+print(int(montantRendu // 20) ,"de 20 €")
+montantRendu = montantRendu % 20
+print(int(montantRendu // 10) ,"de 10 €")
+montantRendu = montantRendu % 10
+print(int(montantRendu // 5) ,"de 5 €")
+montantRendu = montantRendu % 5
+print(int(montantRendu // 2) ,"de 2 €")
+montantRendu = montantRendu % 2
+print(int(montantRendu // 1),"de 1 €")
+montantRendu = montantRendu % 1
+print(int(montantRendu // 0.5) ,"de 50 c")
+montantRendu = montantRendu % 0.5
+print(int(montantRendu // 0.2) ,"de 20 c")
+montantRendu = montantRendu % 0.2
+print(int(montantRendu // 0.1) ,"de 10 c")
+montantRendu = montantRendu % 0.1
+print(int(montantRendu // 0.05) ,"de 5 €")
+montantRendu = montantRendu % 0.05
+print(int(montantRendu // 0.02) ,"de 2 c")
+montantRendu = montantRendu % 0.02
+print(int(montantRendu // 0.01) ,"de 1 c")
+'''
